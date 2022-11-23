6dba24a4046c71561d587b2df2df09057229532b
vatsal-shah02 <vatsalshah1902@gmail.com>
2022-11-23 20:48:21 +0530

    modified db url

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 //mongodb://localhost:27017/GoTypingRegistration
-mongoose.connect("mongodb+srv://user:HCCRlKi19rbNsx1C@cluster0.w0umsg9.mongodb.net/test").then(()=>{
+mongoose.connect(process.env.mongoURI).then(()=>{
     console.log(`connection sucessful`);
 }).catch((e)=>{
     console.log(e);
